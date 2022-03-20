36f1f7420c66ad5f6651e2737f04e8782084c9f9
bongbnbbbbbbbbb <94068731+bongbnbbbbbbbbb@users.noreply.github.com>
2022-03-20 23:02:37 +0900

    UI example

---
+++ GameUI/Assets/mainmenu.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mainmenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void OnClickNewGame()
+    {
+        Debug.Log("새 게임");
+    }
+
+    public void OnClickLoad()
+    {
+        Debug.Log("불러오기");
+    }
+    
+    public void OnClickOption()
+    {
+        Debug.Log("옵션");
+    }
+
+    public void OnClickQuit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+    public void OnClickCustom()
+    {
+        Debug.Log("고객센터");
+    }
+}
